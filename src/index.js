1f02724e3784204665ee1f8d70e44583f886b87b
Jazmine <jazmihic@amazon.com>
2023-04-16 23:17:01 -0500

    priority added

--- src/index.js
+++ src/index.js
@@ -12,13 +12,35 @@ function addingEventListeners() {
 function handleFormSubmit(e) {
   e.preventDefault();
   const task = e.target[0].value;
+  const priorityLevel = parseInt(e.target.priority.value);
 
-  displayTask(task);
+  displayTask(task, priorityLevel);
 }
 
-function displayTask(task) {
+function displayTask(task, priorityLevel) {
   const taskUl = document.getElementById("tasks");
   const taskLi = document.createElement("li");
-  taskLi.textContent = task;
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "x";
+  deleteBtn.addEventListener("click", deleteTask);
+
+  taskLi.textContent = task + " ";
+  taskLi.style.color = getPriorityColor(priorityLevel);
+  taskLi.appendChild(deleteBtn);
   taskUl.appendChild(taskLi);
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
+
+function getPriorityColor(priorityLevel) {
+  if (priorityLevel === 1) {
+    return "red";
+  } else if (priorityLevel === 2) {
+    return "blue";
+  } else {
+    return "green";
+  }
 }
